8ca8a6822b33af986d7932f5bc7384aedcb497b4
ValentinAr7 <valio990210@gmail.com>
2023-11-23 20:54:10 +0200

    phone view for instagram

--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { InstagramData } from "../data";
 import "yet-another-react-lightbox/styles.css";
 import { motion } from "framer-motion";
@@ -6,6 +6,25 @@ import { fadeIn } from "../variants";
 
 const Instagram = () => {
   const { title, btnText, btnIcon, images } = InstagramData;
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <motion.div
       variants={fadeIn("up")}
@@ -21,11 +40,19 @@ const Instagram = () => {
         <div className="container mx-auto">
           <h2 className="h2 max-w-none mb-12">Последвайте ни в инстаграм</h2>
 
-          <img
-            src={require("../assets/tatts.png")}
-            alt="Tattoo"
-            className="w-full h-auto" // This makes the image take the full width and adjusts its height accordingly
-          />
+          {isMobile ? (
+            <img
+              src={require("../assets/phone-tatts.png")}
+              alt="Tattoo for phones"
+              className="w-full h-auto"
+            />
+          ) : (
+            <img
+              src={require("../assets/tatts.png")}
+              alt="Tattoo"
+              className="w-full h-auto"
+            />
+          )}
         </div>
       </section>
     </motion.div>
